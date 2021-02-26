781db06a9afc8f9fee1f59812182e375f6b001b7
anlsim <anlsimental@gmail.com>
2021-02-25 22:44:14 -0800

    creating shop page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/Homepage';
 import {Route} from 'react-router-dom';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
      <Route exact path='/' component={HomePage}/> 
+     <Route  path='/shop' component={ShopPage}/> 
      
     </div>
   );
