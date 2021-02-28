22e76081a3c1e80c5caad28d68f54cdb33435c1e
anlsim <anlsimental@gmail.com>
2021-02-27 19:46:25 -0800

    created sign in with google and styles

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/header';
 import SignInPage from './pages/signInPage/sign-in';
+import {auth} from './firebase/firebase.utils';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+    })
+  }
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  render(){
   return (
     <div>
-      <Header/>
+    <Header currentUser={this.state.currentUser}/>
      <Switch>
      <Route exact path='/' component={HomePage}/> 
      <Route  path='/shop' component={ShopPage}/> 
@@ -20,6 +37,7 @@ function App() {
      
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,8 +3,9 @@ import {Link} from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.scss';
+import {auth} from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -13,7 +14,10 @@ const Header = () => (
             <Link className='option' to='/shop'> SHOP</Link>
         </div>
         <div className='options'>
-            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/contact'>CONTACT</Link>
+            {
+                currentUser ? <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
+            }
         </div>
     </div>
 );
