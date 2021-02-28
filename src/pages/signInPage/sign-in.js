35b3f01f5c3650968b3c505a5e3c0e6a6bc4c7ff
anlsim <anlsimental@gmail.com>
2021-02-28 14:53:14 -0800

    added sign up component

--- src/pages/signInPage/sign-in.js
+++ src/pages/signInPage/sign-in.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp  from '../../components/sign-up/sign-up';
+import './sign-in.scss';
 const SignInPage = ()=> (
     <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
     </div>
 
 );
