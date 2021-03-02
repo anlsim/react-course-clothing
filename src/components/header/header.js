10cf17ff33fd137323c7b3a40880fdac4cf49d79
anlsim <anlsimental@gmail.com>
2021-03-02 10:11:46 -0800

    adding cart-icon component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,22 +5,33 @@ import {connect} from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.scss';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon';
 
 const Header = ({currentUser}) => (
     <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-            </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'> SHOP</Link>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-        <div className='options'>
-            <Link className='option' to='/contact'>CONTACT</Link>
-            {
-                currentUser ? <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
     </div>
+    
+  </div>
 );
 const mapStateToProps = state => (
     {
