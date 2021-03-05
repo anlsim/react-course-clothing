3aa80f2afe3b780e7cddb179b4bbedea54862e52
anlsim <anlsimental@gmail.com>
2021-03-04 21:06:58 -0800

    adding selector library and functionality

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import './cart-icon.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
@@ -12,6 +13,13 @@ const CartIcon = ({toggleCartHidden}) => (
 );
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+  });
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(CartIcon);
