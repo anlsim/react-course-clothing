35f6cf2d1d180a896ef2f00608fbd7609ef89c7a
anlsim <anlsimental@gmail.com>
2021-03-07 14:42:55 -0800

    adding redux selector to checkout page

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import CartItem from '../../components/cart-item/cart-item';
+import { selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
+
 
 import './checkout.scss';
 
-const Checkout = () => (
-    <div className=''>CHECKOUT PAGE</div>
+const Checkout = ({cartItems, total}) => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className ='header-block'>
+                <span>Product</span>
+            </div>
+            <div className ='header-block'>
+                <span>Description</span>
+            </div>
+            <div className ='header-block'>
+                <span>Quantity</span>
+            </div>
+            <div className ='header-block'>
+                <span>Price</span>
+            </div>
+            <div className ='header-block'>
+                <span>Remove</span>
+            </div>
+        </div>
+        {
+            cartItems.map(cartItem =>
+                cartItem.name
+                )
+        }
+        <div className = 'total'>
+            <span>TOTAL: ${total}</span>
+        </div>
+    </div>
+    
 );
-export default Checkout;
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+});
+export default connect(mapStateToProps)(Checkout);
