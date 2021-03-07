62e26150e9a17d80bfc209a8748972414a0e1aa4
anlsim <anlsimental@gmail.com>
2021-03-07 15:36:41 -0800

    implemented redux clear item on chekout page

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -5,10 +5,10 @@ import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import './cart-icon.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'> 0 </span>
+        <span className='item-count'> {itemCount} </span>
     </div>
 );
 const mapDispatchToProps = dispatch => ({
