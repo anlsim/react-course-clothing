c707e1fcc80a34ab037a453cd5a3af82f37f1213
anlsim <anlsimental@gmail.com>
2021-03-09 15:43:40 -0800

    adding memoize

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,11 +1,15 @@
 
 import { createSelector } from 'reselect';
 
+import memoize from 'lodash.memoize';
+
 const selectShop = state => state.shop;
 
-export const selectCollections = createSelector(
-  [selectShop],
-  shop => shop.collections
+export const selectCollections = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  )
 );
 
 export const selectCollectionsForPreview = createSelector(
