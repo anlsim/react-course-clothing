0130fbbc92174cb10e5a83b46764e63d8fdddc23
anlsim <anlsimental@gmail.com>
2021-03-09 16:11:00 -0800

    normalizing data

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,21 +1,17 @@
-
 import { createSelector } from 'reselect';
-
-import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
-export const selectCollections = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
-  )
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
 );
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   collections => Object.keys(collections).map(key => collections[key])
 );
+
 
 export const selectCollection = collectionUrlParam =>
   createSelector(
