90522073a5fdb913cce35818c5a50a99288e7df3
anlsim <anlsimental@gmail.com>
2021-03-28 15:48:59 -0700

    sign up with saga

--- src/components/sign-up/sign-up.js
+++ src/components/sign-up/sign-up.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
+
+import { signUpStart } from '../../redux/user/user.actions';
 
 
 import { SignUpContainer, SignUpTitle } from './sign-up.styles';
@@ -20,84 +23,75 @@ class SignUp extends React.Component{
         }
     }
     handleSubmit = async event => {
-        event.preventDefault();
-    
-        const { displayName, email, password, confirmPassword } = this.state;
-    
-        if (password !== confirmPassword) {
-          alert("passwords don't match");
-          return;
-        }
-    
-        try {
-          const { user } = await auth.createUserWithEmailAndPassword(
-            email,
-            password
-          );
-    
-          await createUserProfileDocument(user, { displayName });
-    
-          this.setState({
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: ''
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      };
-    
-      handleChange = event => {
-        const { name, value } = event.target;
-    
-        this.setState({ [name]: value });
-      };
-    
-    render(){
-        const {displayName, email, password, confirmPassword} = this.state;
-        return(
-            <SignUpContainer>
-            <SignUpTitle>I do not have an account </SignUpTitle>
-            <span>Sign up with your email and password</span>
-            <form className='sign-up-form' onSubmit={this.handleSubmit}>
+      event.preventDefault();
+      const { signUpStart } = this.props;
+      const { displayName, email, password, confirmPassword } = this.state;
+  
+      if (password !== confirmPassword) {
+        alert("passwords don't match");
+        return;
+      }
+  
+      signUpStart({ displayName, email, password });
+    };
+  
+    handleChange = event => {
+      const { name, value } = event.target;
+  
+      this.setState({ [name]: value });
+    };
+  
+    render() {
+      const { displayName, email, password, confirmPassword } = this.state;
+      return (
+        <SignUpContainer>
+          <SignUpTitle>I do not have a account</SignUpTitle>
+          <span>Sign up with your email and password</span>
+          <form className='sign-up-form' onSubmit={this.handleSubmit}>
             <FormInput
-            type='text'
-            name='displayName'
-            value={displayName}
-            onChange={this.handleChange}
-            label='Display Name'
-            required
-          />
-          <FormInput
-            type='email'
-            name='email'
-            value={email}
-            onChange={this.handleChange}
-            label='Email'
-            required
-          />
-          <FormInput
-            type='password'
-            name='password'
-            value={password}
-            onChange={this.handleChange}
-            label='Password'
-            required
-          />
-          <FormInput
-            type='password'
-            name='confirmPassword'
-            value={confirmPassword}
-            onChange={this.handleChange}
-            label='Confirm Password'
-            required
-          />
-          <CustomButton type='submit'>SIGN UP</CustomButton>
-        </form>
-    </SignUpContainer>
-    );
+              type='text'
+              name='displayName'
+              value={displayName}
+              onChange={this.handleChange}
+              label='Display Name'
+              required
+            />
+            <FormInput
+              type='email'
+              name='email'
+              value={email}
+              onChange={this.handleChange}
+              label='Email'
+              required
+            />
+            <FormInput
+              type='password'
+              name='password'
+              value={password}
+              onChange={this.handleChange}
+              label='Password'
+              required
+            />
+            <FormInput
+              type='password'
+              name='confirmPassword'
+              value={confirmPassword}
+              onChange={this.handleChange}
+              label='Confirm Password'
+              required
+            />
+            <CustomButton type='submit'>SIGN UP</CustomButton>
+          </form>
+        </SignUpContainer>
+      );
     }
-}
-
-export default SignUp;
+  }
+  
+  const mapDispatchToProps = dispatch => ({
+    signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+  });
+  
+  export default connect(
+    null,
+    mapDispatchToProps
+  )(SignUp);
